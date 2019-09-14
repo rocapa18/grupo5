c38b8fda20af01b86ab159d21cff8de605c61762
rocapa18 <roberto.pavano@gmail.com>
2019-09-14 18:35:37 -0300

    Resueltos los problemas del home. Quedarìa listo en vista de escritorio

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ particlesJS('particles-js',
         "value": 80,
         "density": {
           "enable": true,
-          "value_area": 800
+          "value_area": 900
         }
       },
       "color": {
--- js/particles.js
+++ js/particles.js
@@ -91,7 +91,7 @@ var pJS = function(tag_id, params){
       detect_on: 'canvas',
       events: {
         onhover: {
-          enable: true,
+          enable: false,
           mode: 'grab'
         },
         onclick: {
